e709c4e824b1bc6f975fc7b55569930d0d392c56
hebounds <hayeservin@gmail.com>
2023-09-28 19:22:28 -0400

    various fixes

--- backend/app.py
+++ backend/app.py
@@ -14,13 +14,12 @@ CORS(app)
 
 @app.route('/plot1', methods=["GET"])
 def plot1(): 
-  # copy/pasted from Bokeh Getting Started Guide
     x = linspace(-6, 6, 100)
     y = cos(x)
     p = figure(width=500, height=500, toolbar_location="below",
                      title="Plot 1")
     p.circle(x, y, size=7, color="firebrick", alpha=0.5)
- 
+
     # following above points: 
     #  + pass plot object 'p' into json_item
     #  + wrap the result in json.dumps and return to frontend
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Axios from 'axios';
 
 function App() {
 
+  // On component load send http get request to flask server to retrieve plot1
   useEffect(() => {
     Axios.get("http://127.0.0.1:5000/plot1").then(resp => 
     {
@@ -17,6 +18,7 @@ function App() {
     <div className="text-center">
       <header className="bg-gunmetal min-h-screen flex flex-col justify-center items-center text-xl text-white">
         <div className="text-3xl font-bold text-coral"> TailwindCSS Example Line </div>
+        <div className="text-sm text-white"> Example Description of Plot </div>
 
         <div class="py-8 px-8 max-w-2xl mx-auto my-8 bg-battle-grey1 rounded-xl shadow-lg space-y-2">
           <div class="py-3 px-3 max-x-2xl my-1 bg-white rounded-xl shadow-lg space-y-2">
